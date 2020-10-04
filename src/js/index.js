7e3e8888140dcff83fc4648a5e85f8902c17c7d8
Frank <frankmckechnie@gmail.com>
2020-10-04 14:33:24 +0100

    Simple example

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import utils from './utils';
+
+document.getElementById('result').innerHTML = `
+    Random generated number with the import utils :) is ${utils.randomIntFromRange(1,100)}
+`;
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,50 +1,54 @@
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: ['./src/js/canvas.js', './src/scss/master.scss'],
+  mode: 'production',
+  entry: ['./src/js/index.js', './src/scss/master.scss'],
   output: {
     path: __dirname + '/dist/',
-    filename: "js/[name].[contentHash].bundle.js",
+    filename: 'js/[name].[contentHash].bundle.js',
   },
   optimization: {
     minimizer: [
       new OptimizeCssAssetsPlugin(),
       new TerserPlugin(),
       new HtmlWebpackPlugin({
-        template: "./src/index.html",
+        template: './src/index.html',
         minify: {
           removeAttributeQuotes: true,
           collapseWhitespace: true,
-          removeComments: true
-        }
-      })
-    ]
+          removeComments: true,
+        },
+      }),
+    ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "/css/[name].[contentHash].css" }),
-    new CleanWebpackPlugin()
+    new MiniCssExtractPlugin({
+      filename: '/css/[name].[contentHash].css',
+    }),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use:[ {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
-        }]
+        ],
       },
       {
         // HTML LOADER
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: 'html-loader',
       },
       {
         test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/i,
@@ -53,7 +57,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               esModule: false,
-            }
+            },
           },
         ],
       },
@@ -61,10 +65,10 @@ module.exports = {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader, //3. Extract css into files
-          "css-loader", //2. Turns css into commonjs
-          "sass-loader" //1. Turns sass into css
-        ]
-      }
-    ]
+          'css-loader', //2. Turns css into commonjs
+          'sass-loader', //1. Turns sass into css
+        ],
+      },
+    ],
   },
 };
